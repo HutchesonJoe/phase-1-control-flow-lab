9fbe1b754b4b9d5af9c37b5943060fca5648e7ad
Joe <hutcheson2001@yahoo.com>
2022-01-11 11:17:58 -0500

    commit tree a0151ba1e9cd7cb3c13e735ec66364f2d6328eb2

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feetNumber){
+  let result
+  if (feetNumber <= 199) {
+    result = `This one is on me!`;
+    } else if (feetNumber > 2500) {
+      result = `No can do.`;
+      }
 
-function ternaryCheckCity(){
+  else if (feetNumber > 2000) {
+   result = `I will gladly take your thirty bucks.`;}
+     
+  
+  console.log(result)
+  return result;
+  }
+  
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+scuberGreetingForFeet(188);
+
+function ternaryCheckCity(city){
+    return city==="NYC" ? "Ok, sounds good." : "No go.";
+    
+    }
+
+function switchOnCharmFromTip(tipAmount){
+  if (tipAmount==="generous") {
+    return "Thank you so much."
+  } else if (tipAmount==='not as generous') {
+    return "Thank you."
+  } else if (tipAmount==="thanks for everything") {
+    return "Bye."
+  }
   // Write your code here!
 }
